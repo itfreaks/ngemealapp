6fa38a9da681a6cc42df2ca7a14fc3816d191326
AndrianAdam <andrian.adam@gmail.com>
2021-07-27 10:35:54 +0700

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -44,5 +44,7 @@ module.exports = merge(common, {
       },
     ],
   },
-  plugins: [new BundleAnalyzerPlugin()],
+  plugins: [new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+  })],
 });
